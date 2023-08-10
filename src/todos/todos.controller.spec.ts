7005f9fe81b77e14208c77deb53e0579569c98eb
Hridayesh Akhand <hridayesh.akhand@gmail.com>
2023-08-10 17:13:43 +0900

    dockerfile + todos test

--- src/todos/todos.controller.spec.ts
+++ src/todos/todos.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TodosController } from './todos.controller';
+import { TodosService } from './todos.service';
 
 describe('TodosController', () => {
-  let controller: TodosController;
+  let todosController: TodosController;
+  let todosService: TodosService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TodosController],
+      providers: [TodosService],
     }).compile();
 
-    controller = module.get<TodosController>(TodosController);
+    todosService = module.get<TodosService>(TodosService);
+    todosController = module.get<TodosController>(TodosController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of todos', async () => {
+      const result = [
+        {
+          dueDate: {
+            _seconds: 1548381600,
+            _nanoseconds: 0,
+          },
+          name: 'create test',
+        },
+        {
+          name: 'test',
+          dueDate: {
+            _seconds: 1691766000,
+            _nanoseconds: 410000000,
+          },
+        },
+      ];
+
+      expect(await todosController.findAll()).toBe(result);
+    });
   });
 });
